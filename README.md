056b3ff89d305654637c84ed2b9ad3d393ec699f
afreeland <afreeland@gmail.com>
2021-11-09 23:29:58 -0500

    Improved crawler and finally added readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Pokedex_ML
+This project aims to allow for a real life pokedex.  I've put it off far too long and decided to pick it back up.
+
+
+
+## Crawling images
+Nobody wants to manually fetch images for classification training...so this helps automate that
+
+You can simply run the following
+```
+python crawler.py pikachu
+```
+
+This will automatically start to crawl Google images and begin fetching images for `pikachu`.  Additionally, there are some other searches that are automated for `pikachu plush` and `pikachu cards`.  The hope is that you can simply point your pokedex at many different styles of objects and be able to identify the proper Pokemon.
+
+Things still needing to be done:
+- Include Bing images (which also means prefixing data sets)
+- Automate for *ALL* the different pokemon..but lets start small instead of killing image search at the moment.
+
--- crawler.py
+++ crawler.py
@@ -1,53 +1,77 @@
-from icrawler.builtin  import GoogleImageCrawler
+from icrawler.builtin import GoogleImageCrawler
 import os
 import sys
+import string
 import subprocess
+import shutil
 from PIL import Image
 
 # Easy coaleasce function
-def coalesce(*arg):
-  for el in arg:
-    if el is not None:
-      return el
-  return None
 
+
+def coalesce(*arg):
+    for el in arg:
+        if el is not None:
+            return el
+    return None
+
+# Check that the file is an image and that it is not corrupt junk
+
+
+def is_img(filename):
+    try:
+        i = Image.open(filename)
+        return i.format == 'JPEG' or i.format == 'JPG' or i.format == 'PNG'
+    except IOError:
+        return False
+
+
+print(sys.argv)
 # Get label to associate (also used as foldername)
 label = sys.argv[1]
 
 # Get the search term to crawl against or default to label
 searchTerm = label
 
-# Set the directory where we should place our images from crawler
-imageDirectory = 'Image_Data/' + label
+
+imageTypes = [label, label + ' cards', label + ' plush']
 
 
-# Check if the directory already exists
-if not os.path.exists(imageDirectory):
+for imageType in imageTypes:
+    trimmedType = imageType.translate(str.maketrans('', '', string.whitespace))
+
+    # Set the directory where we should place our images from crawler
+    imageDirectory = 'Image_Data/' + trimmedType
+
+    # Check if the directory already exists
+    if not os.path.exists(imageDirectory):
         # Directory doesnt exist so we need to create it
         os.mkdir(imageDirectory)
 
-# Use our crawler
-googleCrawler = GoogleImageCrawler(parser_threads=2, downloader_threads=4,
-                            storage={'root_dir': imageDirectory})
-googleCrawler.crawl(
-    keyword=searchTerm, max_num=1000,
-    date_min=None, date_max=None,
-    min_size=(200,200), max_size=None
+    # Use our crawler
+    googleCrawler = GoogleImageCrawler(parser_threads=2, downloader_threads=4,
+                                       storage={'root_dir': imageDirectory})
+    googleCrawler.crawl(
+        keyword=searchTerm, max_num=1000,
+        min_size=(200, 200), max_size=None
     )
 
-# Check that the file is a jpeg and that it is not corrupt junk
-def is_jpg(filename):
-    try:
-        i=Image.open(filename)
-        return i.format =='JPEG'
-    except IOError:
-        return False
+    for root, dirs, files in os.walk(imageDirectory):
+        for currentFile in files:
+            ext = '.jpg'
+            imagePath = root + '/' + currentFile
+            print(imagePath)
+            if (not is_img(imagePath)):
+                print('not jpg')
+                os.remove(os.path.join(root, currentFile))
+            else:
+                # Only move files that are not from our original arg (ie: cards, plush)
+                if imageType != label:
+                    shutil.move(imagePath, './Image_Data/' +
+                                label + '/' + trimmedType + "_" + currentFile)
 
-for root, dirs, files in os.walk(imageDirectory):
-		for currentFile in files:
-			ext = '.jpg'
-			imagePath =  root + '/' + currentFile
-			print(imagePath)
-			if (not is_jpg(imagePath)):
-			 	print('not jpg')
-			 	os.remove(os.path.join(root, currentFile))
+    # Delete our extra folders once we moved all files into the primary folder
+    if imageType != label:
+        if os.path.exists(imageDirectory):
+           # removing the file using the os.remove() method
+            os.rmdir(imageDirectory)
